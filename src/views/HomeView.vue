58f6e36222db1c55a8ef8d623412e724baad73a9
AJ0267 <aj0386057@gmail.com>
2024-10-30 09:28:09 +0530

    add disclaimer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,43 +1,49 @@
 <template>
   <div class="flex flex-wrap gap-4">
     <Card
-      v-for="(card, index) in cards" 
-      :key="index" 
+      v-for="(card, index) in cards"
+      :key="index"
       :title="card.title"
       :description="card.description"
       @onClick="changeRoute(card.route)"
       class="bg-darkGray text-brightOrange"
     />
   </div>
+  <br />
+  <div class="text-center">
+    <p class="mt-2 text-sm text-darkGray">
+      This app is still in progress; additional data will be added soon.
+    </p>
+  </div>
 </template>
 
 <script>
-import { useRouter } from 'vue-router'; 
+import { useRouter } from "vue-router";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   setup() {
-    const router = useRouter(); 
+    const router = useRouter();
 
-    const cards = [ 
+    const cards = [
       {
         title: "Search",
         description: "Search for single Kanji data.",
-        route: '/search',
+        route: "/search",
       },
       {
         title: "Kanji List",
         description: "Lists of Kanji.",
-        route: '/list',
+        route: "/list",
       },
     ];
 
     const changeRoute = (route) => {
       console.log(`Navigating to ${route}...`);
-      router.push(route); 
+      router.push(route);
     };
 
-    return { cards, changeRoute }; 
+    return { cards, changeRoute };
   },
 };
 </script>
